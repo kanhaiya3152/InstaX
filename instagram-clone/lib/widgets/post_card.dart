b6d9cf21dc1effbe465eaca719bc96dfe92057ba
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-01-19 19:26:52 +0530

    add user details

--- instagram-clone/lib/widgets/post_card.dart
+++ instagram-clone/lib/widgets/post_card.dart
@@ -9,9 +9,38 @@ class PostCard extends StatelessWidget {
     return Container(
       color: mobileBackgroundColor,
       child: ListTile(
-        leading: CircleAvatar(
-          radius: ,
-          backgroundImage: NetworkImage('https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg'),
+        leading: const CircleAvatar(
+          radius: 18,
+          backgroundImage: NetworkImage(
+              'https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg'),
+        ),
+        title: Text('Username'),
+        subtitle: Text('place'),
+        trailing: IconButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (ctx) => Dialog(
+                        child: ListView(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      shrinkWrap: true,
+                      children: [
+                        'Delete',
+                      ]
+                          .map(
+                            (e) => InkWell(
+                              onTap: () {},
+                              child: Container(
+                                padding:const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 12),
+                                child: Text(e),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    )));
+          },
+          icon: const Icon(Icons.more_vert),
         ),
       ),
     );
