0e908a626595da2746826729ba9f91557c001d92
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-01-22 22:39:47 +0530

    call profile screen

--- instagram-clone/lib/utils/global_variable.dart
+++ instagram-clone/lib/utils/global_variable.dart
@@ -1,15 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_demo/screens/add_post_screen.dart';
 import 'package:insta_demo/screens/feed_screen.dart';
+import 'package:insta_demo/screens/profile_screen.dart';
 import 'package:insta_demo/screens/search_screen.dart';
 import 'package:insta_demo/widgets/post_card.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
+List<Widget> homeScreenItems = [
   FeedScreen(),
   SearchScreen(),
   AddPostScreen(),
   Text('like'),
-  Text('profile'),
+  ProfileScreen(uid: FirebaseAuth.instance.currentUser!.uid,),
 ];
